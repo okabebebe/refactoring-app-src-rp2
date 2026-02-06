00d9fa7d23b9ff723933e70963cedc91937ee709
h-okabe <h-okabe@PLPC34>
2026-02-06 10:29:09 +0900

    fix:ConstantValue.java 新規作成
    【目的】保守性、正確性向上のため
    定数化された値をまとめたクラスを新規作成

---
+++ refactoring_app_okabe/src/jp/co/sss/crud/util/ConstantValue.java
@@ -0,0 +1,19 @@
+package jp.co.sss.crud.util;
+
+/**
+ * 設定値をまとめたクラス
+ *
+ * @author okabe
+ *
+ */
+
+public class ConstantValue {
+	
+	/** インスタンス化を禁止 */
+	private ConstantValue(){
+	}
+	
+	/** メニュー一覧表示にてシステムを終了させる選択肢の番号 */
+	public static final int menuExitNum = 7;
+
+}
